83b41293df76392b568f6f6228e629016943e5ec
Tomm Smith <root.packet@gmail.com>
2014-10-19 19:47:55 -0400

    Dealt with the special characters *,@,],\ and noted on a few points of general use and how to ensure characters are not interpreted as special.

--- encoding/encoding.sh
+++ encoding/encoding.sh
@@ -1,10 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # NOTES:
 # - Special characters that must be escaped exclusively are: * @ \ ]
+#   These special conditions are dealt with as grammatical representations in the index, 
+#   and the value of both the ASCII table and the BINARY table. 
+#   EG. 
+#     * - XK_asterisk
+#     @ - XK_at
+#     \ - XK_backslash
+#     ] - XK_bracketright
+#
+#   See /usr/include/keysymdefs.h for details of identifier names.
+#   Also, said grammatical representations are maintained in both 
+#   encode() as well as decode() for programmatic symmetry. 
+#
+# Notes:
+#   When passing data, if using single quotes you only have to deal with single quotes carefully.
+#   EG. "encode 'She'"'"'s going to go to the show.'"
+#   It is suggested to always use single quotes when passing the data for integrity preservation and ensuring 
+#   success. 
+
+
+
 
 # Our transliteration tables
 source header.sh
+
 
 # encode() - A function for encoding ASCII to a numeric representation of binary
 #
@@ -22,43 +43,98 @@ function encode() {
   for i in $(seq 0 $((str_len - 1)))
   do
     char=${char_str:step_cnt:1}
-    bin=${CHAR_TBL["$char"]}
 
+    case "$char" in
+      '*')
+        bin="${CHAR_TBL['XK_asterisk']}"
+        ;;
+
+      '@')
+        bin="${CHAR_TBL['XK_at']}"
+        ;;
+
+      '\')
+        bin="${CHAR_TBL['XK_backslash']}"
+        ;;
+
+      ']')
+        bin="${CHAR_TBL['XK_bracketright']}"
+        ;;
+
+      *)
+        bin="${CHAR_TBL[$char]}"
+        ;;
+    esac
+
+    encoded_str+="$bin"
+    
     let step_cnt++
     let next_step++
-  
-    encoded_str="${encoded_str}${bin}"
   done
 
-  echo $encoded_str
+  echo "$encoded_str"
   return 0
 }
-
-
 
 
 # decode() - A function for decoding a numeric binary stream to ASCII characters.
 #
 # @arg $1 - A binary str
 #
-# Return: An ASCII string upon success, 1 upon failure.
+# Return: An ASCII string upon success. 
+#         - 1 upon failure.
+#         - 2 upon invalid character.
+#         - 255 upon malformed bit stream.
 function decode() {
   [ ! "$1" ] && return 1
-  # bit length check 
   
   strm="$1"
   strm_len=${#1}
-  byte_cnt=$(echo "$strm_len / 8" | bc)
   decoded_str=""
+  
+  # bit length check 
+  byte_cnt=$(echo "scale=2; $strm_len / 8" | bc)
+  whole_num=$(echo "print $byte_cnt % 1" | python)
 
-  #if [ $(( $tr % 2 )) -gt 1 ] ;then
-  #  echo "ERROR: decode(): Provided byte stream is not a multiple of 8."
-  #fi
+  if [ $whole_num != 0.0 ] ;then
+    echo "ERROR: decode(): Bit stream is not a multiple of 8."
+    return 255
+  fi
 
   for ((i=0; i<strm_len; i+=8))  
   do
     ind="${strm:i:8}"
-    decode_str+="${BIN_TBL[$ind]}"
+   
+    # Character does not exist in the table
+    if [ "${BIN_TBL[$ind]}" == '' ] ;then
+      echo "ERROR: decode(): Encoded character does not exist in the binary table."
+      return 2
+    fi
+
+    # Deal with our special characters
+    case "${BIN_TBL[$ind]}" in
+      'XK_asterisk')
+        char='*'
+        ;;
+
+      'XK_at')
+        char='@'
+        ;;
+
+      'XK_backslash')
+        char='\'
+        ;;
+
+      'XK_bracketright')
+        char=']'
+        ;;
+
+      *)
+        char="${BIN_TBL[$ind]}"
+        ;;
+    esac
+
+    decode_str+="${char}"
   done    
 
   echo "$decode_str"
